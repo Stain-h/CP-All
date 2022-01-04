52c4d1befa00c82473e242bb9ceb1cf049769971
Stain-h <sangheon1646@naver.com>
2022-01-04 20:33:34 +0900

    Feat : 기본 라우팅 설정

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Page from './pages/Page'
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/page" element={<Page />} ></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
