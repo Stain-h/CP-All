5dd65d89f535f53d5485890739585d5a53fab42b
Stain-h <sangheon1646@naver.com>
2022-01-04 23:24:49 +0900

    Feat : 지도 연동

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+const { naver } = window;
 
 export default function Home() {
+  useEffect(() => {
+    const container = document.getElementById("cp-map")
+    const mapOptions = {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10
+    };
+    const map = new naver.maps.Map(container, mapOptions);
+  }, [])
+  
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
       Home
+      <div id="cp-map" style={{ width:"100%", height:"100vh" }}></div>
     </div>
   )
 }
